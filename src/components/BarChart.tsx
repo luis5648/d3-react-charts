6755ae0a14edaa093c4abc4d52090506d18f0d86
Luis Roman <porta5648@gmail.com>
2024-10-27 10:05:06 -0600

    added barchart component

---
+++ src/components/BarChart.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+type Props = {};
+
+const BarChart = (props: Props) => {
+  return (
+    <>
+      <Typography variant="h4">test</Typography>
+      <Typography variant="h4">test from another component</Typography>
+    </>
+  );
+};
+
+export default BarChart;
