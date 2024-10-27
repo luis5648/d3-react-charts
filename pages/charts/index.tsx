feaf48f9eed8c60b5b627980dfecb7355f724717
Luis Roman <porta5648@gmail.com>
2024-10-27 09:31:30 -0600

    added next and charts directory

---
+++ pages/charts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Typography,Container} from '@mui/material'
+
+type Props = {}
+
+const index = (props: Props) => {
+  return (
+    <Container>
+    <Typography variant="h4">test</Typography>
+    </Container>
+    
+  )
+}
+
+export default index
