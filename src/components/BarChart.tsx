057dc4a218a4e732ab4f62f6b963cd138a56d7bf
Luis Roman <porta5648@gmail.com>
2024-11-01 19:08:03 -0600

    Adding d3 logic to useEffect

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,17 +1,55 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useRef, useEffect } from "react";
+import { Grid, Typography } from "@mui/material";
+import * as d3 from 'd3';
 
 type Props = {};
 
 const BarChart = (props: Props) => {
+  const svgRef = useRef<SVGSVGElement | null>(null)
+  const width = 928;
+  const height = width;
+
+  const chartDefinition = {
+    width: width,
+    height: height,
+    innerRadius: 180,
+    outerRadius: Math.min(width, height) / 2,
+  }
+
+  useEffect(() => {
+    const series = d3.stack().keys()
+
+
+    //append graph elements to svg
+    d3.select(svgRef.current)
+      .append("rect")
+      .attr("width", 100)
+      .attr("height", 100)
+      .attr("fill", "green")
+
+  }, [])
+
+
+
+
+
   return (
     <>
       <Typography variant="h4">BarChart sample</Typography>
-      <svg>
-      </svg>
+
+      <Grid container>
+
+        <Grid item mt={5}>
+          <svg ref={svgRef}>
+          </svg >
+        </Grid>
+      </Grid>
+
+
     </>
   );
 };
+
 
 
 export default BarChart;
