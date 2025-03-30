46877c0d39899b9eea1faa488cf77216bd04a1e7
Luis Roman <porta5648@gmail.com>
2025-03-30 11:09:26 -0600

    cleaned barchart component

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -67,23 +67,6 @@ const BarChart = (props: Props) => {
       g.append("g")
         .call(d3.axisLeft(yScale));
 
-
-      // const svg = selection
-      //   .selectAll("rect")
-      //   .data(data)
-      //   .enter()
-      //   .append("rect")
-      //   .attr("height", function (d, i) {
-      //     return d * 15;
-      //   })
-      //   .attr("width", "50")
-      //   .attr("x", function (d, i) {
-      //     return 60*i;
-      //   })
-      //   .attr("y", function (d, i) {
-      //     return 300 - (d * 15);
-      //   })
-      //   .attr("fill", "blue");
     }
   }, [selection]);
 
